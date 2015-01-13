813d0b63312555a81b490c2f598b1775ee51b1f7
bernar_w <bernar_w@epitech.eu>
2015-01-13 11:30:43 +0100

    add JSON lib and beginning of LoginActivity

---
+++ src/androidTest/java/com/intradroid/dt/intradroid/RequestAPI.java
@@ -0,0 +1,59 @@
+package com.intradroid.dt.intradroid;
+
+/**
+ * Created by bernar_w on 13/01/2015.
+ */
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class RequestAPI {
+
+    public static final void testQuery(){
+        String url = "https://epitech-api.herokuapp.com/login";
+    try {
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        //con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "login=bernar_w&password=*=mvthd{";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    } catch (Exception e) {
+        return ;
+    }
+    }
+}
