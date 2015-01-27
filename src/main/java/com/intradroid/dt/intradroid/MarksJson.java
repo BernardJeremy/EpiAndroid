41b9ecf049ffa70e03e86f2ef266de3a069a5536
bernar_w <bernar_w@epitech.eu>
2015-01-27 16:28:48 +0100

    add grade JSON class

---
+++ src/main/java/com/intradroid/dt/intradroid/MarksJson.java
@@ -0,0 +1,25 @@
+package com.intradroid.dt.intradroid;
+
+/**
+ * Created by bernar_w on 27/01/2015.
+ */
+public class MarksJson {
+
+    private Notes[] notes;
+
+    public MarksJson() {
+
+    }
+
+    public Notes[] getNotes ()
+    {
+        return notes;
+    }
+
+    public void setNotes (Notes[] notes)
+    {
+        this.notes = notes;
+    }
+
+
+}
---
+++ src/main/java/com/intradroid/dt/intradroid/Notes.java
@@ -0,0 +1,131 @@
+package com.intradroid.dt.intradroid;
+
+/**
+ * Created by bernar_w on 27/01/2015.
+ */
+public class Notes {
+
+    private String title;
+
+    private String final_note;
+
+    private String correcteur;
+
+    private String codeacti;
+
+    private String scolaryear;
+
+    private String codeinstance;
+
+    private String comment;
+
+    private String date;
+
+    private String titlemodule;
+
+    private String codemodule;
+
+    public Notes() {
+
+    }
+
+    public String getTitle ()
+    {
+        return title;
+    }
+
+    public void setTitle (String title)
+    {
+        this.title = title;
+    }
+
+    public String getFinal_note ()
+    {
+        return final_note;
+    }
+
+    public void setFinal_note (String final_note)
+    {
+        this.final_note = final_note;
+    }
+
+    public String getCorrecteur ()
+    {
+        return correcteur;
+    }
+
+    public void setCorrecteur (String correcteur)
+    {
+        this.correcteur = correcteur;
+    }
+
+    public String getCodeacti ()
+    {
+        return codeacti;
+    }
+
+    public void setCodeacti (String codeacti)
+    {
+        this.codeacti = codeacti;
+    }
+
+    public String getScolaryear ()
+    {
+        return scolaryear;
+    }
+
+    public void setScolaryear (String scolaryear)
+    {
+        this.scolaryear = scolaryear;
+    }
+
+    public String getCodeinstance ()
+    {
+        return codeinstance;
+    }
+
+    public void setCodeinstance (String codeinstance)
+    {
+        this.codeinstance = codeinstance;
+    }
+
+    public String getComment ()
+    {
+        return comment;
+    }
+
+    public void setComment (String comment)
+    {
+        this.comment = comment;
+    }
+
+    public String getDate ()
+    {
+        return date;
+    }
+
+    public void setDate (String date)
+    {
+        this.date = date;
+    }
+
+    public String getTitlemodule ()
+    {
+        return titlemodule;
+    }
+
+    public void setTitlemodule (String titlemodule)
+    {
+        this.titlemodule = titlemodule;
+    }
+
+    public String getCodemodule ()
+    {
+        return codemodule;
+    }
+
+    public void setCodemodule (String codemodule)
+    {
+        this.codemodule = codemodule;
+    }
+}
